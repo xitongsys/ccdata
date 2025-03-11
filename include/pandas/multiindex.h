5fd03920c05ac2e0f318a3ae60f47d4830ebdc38
xitongsys <xitongsys@gmail.com>
2025-03-12 06:33:39 +0800

    ..

--- include/pandas/multiindex.h
+++ include/pandas/multiindex.h
@@ -13,35 +13,16 @@
 
 namespace pandas {
 
-template <size_t level, class T>
-class MultiIndex {
-public:
-    MultiIndex(const Array<T>& vs)
-        : values(vs)
-    {
-    }
+template <size_t, class... Ts>
+class MultiIndex;
 
-    std::tuple<T> iloc(int i)
-    {
-        return std::tuple<T>(values.iloc(i));
-    }
-
-    template <size_t>
-    Array<T>& get_level_values() { }
-
-    template <>
-    Array<T>& get_level_values<level>()
-    {
-        return values;
-    }
-
-    Array<T> values;
-};
+template <size_t level>
+class MultiIndex<level> { };
 
 template <size_t level, class T, class... Ts>
 class MultiIndex<level, T, Ts...> : public MultiIndex<level + 1, Ts...> {
 public:
-    MultiIndex(const Array<T>& vs, const Array<Ts>& vss...)
+    MultiIndex(const Array<T>& vs, const Array<Ts>&... vss)
         : values(vs)
         , MultiIndex<level + 1, Ts...>(vss...)
     {
@@ -49,9 +30,14 @@ public:
 
     std::tuple<T, Ts...> iloc(int i)
     {
+        const size_t n_tail = sizeof...(Ts);
         std::tuple<T> t(values.iloc(i));
-        std::tuple<Ts...> ts = MultiIndex<level + 1, Ts...>::iloc(i);
-        return std::tuple_cat(t, ts);
+        if constexpr (n_tail > 0) {
+            std::tuple<Ts...> ts = MultiIndex<level + 1, Ts...>::iloc(i);
+            return std::tuple_cat(t, ts);
+        } else {
+            return t;
+        }
     }
 
     template <size_t>
@@ -66,7 +52,7 @@ public:
     Array<T> values;
 };
 
-template <class T>
-class MultiIndexA : public MultiIndex<0, T> { };
+template <class... Ts>
+class MultiIndexA : public MultiIndex<0, Ts...> { };
 
 }
