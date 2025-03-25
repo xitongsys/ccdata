9553192f841c98763597a2520be050def0c1d367
xitongsys <xitongsys@gmail.com>
2025-03-25 14:44:27 +0800

    ..

--- tests/test_datetime.cc
+++ tests/test_datetime.cc
@@ -17,8 +17,17 @@ using namespace std::chrono;
 
 void test_datetime()
 {
-    Datetime dt(2025,1,2,3,4,5);
-    cout << dt << endl;
+    Datetime dt(2025, 1, 2, 3, 4, 5);
+    assert(dt.to_string() == "2025-01-02 03:04:05");
+
+    Datetime dt2;
+    assert(dt2.to_string() == "NaT");
+
+    Datetime dt3;
+    dt3.strptime("20250102030405", "%Y%m%d%H%M%S");
+    assert(dt3.to_string() == "2025-01-02 03:04:05");
+
+    cout << "[PASS] test_datetime" << endl;
 }
 
 int main()
